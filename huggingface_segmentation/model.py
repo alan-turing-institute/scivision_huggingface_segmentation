9c7570722e7c4d515e419ebde773e2de9dc86e34
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-05-17 14:39:20 +0100

    add microsoft beit models

--- huggingface_segmentation/model.py
+++ huggingface_segmentation/model.py
@@ -1,7 +1,13 @@
 import numpy as np
 from PIL import Image
 import torch
-from transformers import DetrFeatureExtractor, DetrForSegmentation
+from transformers import (DetrFeatureExtractor,
+                          DetrForSegmentation,
+                          DPTFeatureExtractor,
+                          DPTForSemanticSegmentation,
+                          BeitFeatureExtractor,
+                          BeitForSemanticSegmentation
+                          )
 
 
 def tidy_predict(self, image: np.ndarray) -> str:
@@ -25,6 +31,12 @@ def build_detr_model(model_name: str):
 def build_dpt_model(model_name: str):
     model = DPTForSemanticSegmentation.from_pretrained(model_name)
     features = DPTFeatureExtractor.from_pretrained(model_name)
+    return model, features
+    
+    
+def build_beit_model(model_name: str):
+    model = BeitForSemanticSegmentation.from_pretrained(model_name)
+    features = BeitFeatureExtractor.from_pretrained(model_name)
     return model, features
 
     
@@ -55,9 +67,24 @@ class Intel_dpt_large_ade:
         return tidy_predict(self, image)
         
         
-# from .model import Intel_dpt_large_ade
-# from .model import microsoft_beit_base_finetuned_ade_640_640
-# from .model import microsoft_beit_large_finetuned_ade_640_640
+class microsoft_beit_base_finetuned_ade_640_640:
+    def __init__(self):
+        self.model_name = 'microsoft/beit-base-finetuned-ade-640-640'
+        self.pretrained_model, self.feature_extractor = build_beit_model(self.model_name)
+
+    def predict(self, image: np.ndarray) -> np.ndarray:
+        return tidy_predict(self, image)
+    
+class microsoft_beit_large_finetuned_ade_640_640:
+    def __init__(self):
+        self.model_name = 'microsoft/beit-large-finetuned-ade-640-640'
+        self.pretrained_model, self.feature_extractor = build_beit_model(self.model_name)
+
+    def predict(self, image: np.ndarray) -> np.ndarray:
+        return tidy_predict(self, image)
+        
+
+
 # from .model import nvidia_segformer_b0_finetuned_ade_512_512
 # from .model import nvidia_segformer_b5_finetuned_ade_640_640
 # from .model import nvidia_segformer_b4_finetuned_ade_512_512
